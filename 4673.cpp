910014dd6aaade7c327efb1f1a090bb7a6341ad0
siy93 <dlsduq5996@naver.com>
2017-01-03 21:43:47 +0900

    ACM-ICPC

--- 4673.cpp
+++ 4673.cpp
@@ -1,34 +1,30 @@
+#include<iostream>
 #include<cstdio>
 using namespace std;
 
+int tempmaker(int num){
+	int sum = 0;
+	sum += num;
+	if (num < 10){ return sum + num; }
+	while (1){
+		if (num / 10 == 0){ return sum + num; }
+		sum += num % 10;
+		num /= 10;
+	}
+}
+
 int main(void)
 {
-	int num[9];
-	for (int i = 1; i <= 8; i++){
-		scanf("%d", &num[i]);
+	int S[10001] = { 0 }, temp;
+	S[1] = false;
+	for (int i = 1; i <= 10000; i++)
+	{
+		temp = tempmaker(i);
+		if (temp <= 10000)
+			S[temp] = true;
 	}
-	for (int i = 1; i <= 8; i++){
-		if (num[i] == i){
-			continue;
-		}
-		else if (num[i] == 8){
-			break;
-		}
-		else{
-			printf("%s", "mixed");
-			return 0;
-		}
-		printf("%s", "ascending");
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (S[i] == false)printf("%d\n", i);
 	}
-	for (int i = 8; i >= 1; i--){
-		if (num[i] == i){
-			continue;
-		}
-		else{
-			printf("%s", "mixed");
-			return 0;
-		}
-		printf("%s", "descending");
-	}
-	return 0;
 }
