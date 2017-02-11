af1dad767acf650be13a77ee883ff7c18b5bee6e
siy93 <dlsduq5996@naver.com>
2017-02-11 14:23:11 +0900

    Baekjoon_11051

--- 11051.cpp
+++ 11051.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int N, K, binoCoeff[MAX][MAX];
 int main() {
 	cin >> N >> K;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= min(i, K); j++) {
+	for (int i = 1; i <= N+1; i++) {
+		for (int j = 1; j <= min(i, K+1); j++) {
 			if(j == 1 || j==i){
 				binoCoeff[i][j] = 1;
 			}
@@ -16,7 +16,11 @@ int main() {
 			}
 		}
 	}
-	cout << binoCoeff[N][K];
+	cout << binoCoeff[N+1][K+1];
 	return 0;
 }
 
+
+//*이항 계수 메모이제이션 (동적계획법) 으로 구현한코드
+//반복 연산이 줄어 O(nk)에 풀 수 있다.
+//for문의 범위를 잘 생각해야한다.
