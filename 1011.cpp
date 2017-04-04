ecc843cd9b59e943e048e8e29cce26da7e06f812
In Yeop Song <songin1953@gmail.com>
2017-04-04 22:03:18 +0900

    Baekjoon_1001

---
+++ 1011.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+ 
+int main(){
+    int n;
+    unsigned int a,b,x;
+    unsigned int k;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%u%u",&a,&b);
+        x=b-a;
+        k=0;
+        for(unsigned int i=1;;i++){
+            k+=i;
+            if(x<=k){
+                printf("%u
+",2*i-1);
+                break;
+            }
+            k+=i;
+            if(x<=k){
+                printf("%u
+",2*i);
+                break;
+            }
+        }
+    }
+}
