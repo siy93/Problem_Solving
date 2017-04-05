17e6334d514aaae7df7cf72aee8bbfe71535e5e6
In Yeop Song <songin1953@gmail.com>
2017-04-05 23:21:04 +0900

    Baekjoon_1011

--- 1011.cpp
+++ 1011.cpp
@@ -1,27 +1,26 @@
-#include <cstdio>
- 
+#include <iostream>
+using namespace std;
+
 int main(){
-    int n;
-    unsigned int a,b,x;
-    unsigned int k;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%u%u",&a,&b);
-        x=b-a;
-        k=0;
-        for(unsigned int i=1;;i++){
-            k+=i;
-            if(x<=k){
-                printf("%u
-",2*i-1);
-                break;
-            }
-            k+=i;
-            if(x<=k){
-                printf("%u
-",2*i);
-                break;
-            }
-        }
-    }
+	int n;
+	unsigned int a, b, x;
+	unsigned int k;
+	cin >> n;
+	for (int i = 0; i<n; i++){
+		cin >> a >> b;
+		x = b - a;
+		k = 0;
+		for (unsigned int i = 1;; i++){
+			k += i;
+			if (x <= k){
+				cout<<2*i-1<<endl;
+				break;
+			}
+			k += i;
+			if (x <= k){
+				cout<<2*i<<endl;
+				break;
+			}
+		}
+	}
 }
